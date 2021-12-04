2e948bb8497a7601ca1dc38b8ea30d79a377b851
IIIWMWIII <surofyt1@gmail.com>
2021-12-04 20:52:47 +0100

    Feladat kesz

--- MintaZH/MintaZH/Form1.Designer.cs
+++ MintaZH/MintaZH/Form1.Designer.cs
@@ -29,13 +29,45 @@ namespace MintaZH
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.cbEv = new System.Windows.Forms.ComboBox();
+            this.Excelbe_ment = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // cbEv
+            // 
+            this.cbEv.FormattingEnabled = true;
+            this.cbEv.Location = new System.Drawing.Point(256, 47);
+            this.cbEv.Name = "cbEv";
+            this.cbEv.Size = new System.Drawing.Size(121, 21);
+            this.cbEv.TabIndex = 0;
+            // 
+            // Excelbe_ment
+            // 
+            this.Excelbe_ment.Location = new System.Drawing.Point(548, 47);
+            this.Excelbe_ment.Name = "Excelbe_ment";
+            this.Excelbe_ment.Size = new System.Drawing.Size(75, 23);
+            this.Excelbe_ment.TabIndex = 1;
+            this.Excelbe_ment.Text = "button1";
+            this.Excelbe_ment.UseVisualStyleBackColor = true;
+            this.Excelbe_ment.Click += new System.EventHandler(this.Excelbe_ment_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.Excelbe_ment);
+            this.Controls.Add(this.cbEv);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.ComboBox cbEv;
+        private System.Windows.Forms.Button Excelbe_ment;
     }
 }
 
